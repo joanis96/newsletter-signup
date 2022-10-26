3091e1f379090ae04b5e459f7dccbabffd1f2c5c
joanis96 <57779348+joanis96@users.noreply.github.com>
2022-10-26 15:14:17 -0400

    A lot has already been added

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+// to be able to use img and css stylesheet
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var email = req.body.email;
+
+    res.send(console.log("first Name is: " + firstname, "last Name is :" + lastname, "email is: " + email));
+    
+})
+
+app.listen(3000, function() {
+    console.log("Successfully loaded on port 3000 !");
+});
